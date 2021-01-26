31e8de155344d1213a124afa46cf62255914b681
wriggyj <67104461+wriggy@users.noreply.github.com>
2021-01-26 12:03:46 +0000

    Create GetAllOpportunities API using database view

--- Handler.cs
+++ Handler.cs
@@ -8,36 +8,46 @@ using MySql.Data.MySqlClient;
 
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
+// ToDo date conversion, handle Nulls //
+
 namespace AwsDotnetCsharp
 {
     public class Handler
     {
-        public APIGatewayProxyResponse GetOpportunity(APIGatewayProxyRequest request)
-        {
-            string oppId = request.PathParameters["id"];
-            LambdaLogger.Log("get opportunity - db request " + oppId);
+        private string dbHost = Environment.GetEnvironmentVariable("DB_HOST");
+        private string dbUser = Environment.GetEnvironmentVariable("DB_USER");
+        private string dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD");
+        private string dbName = Environment.GetEnvironmentVariable("DB_NAME");
 
-            string dbHost =  Environment.GetEnvironmentVariable("DB_HOST");
-            string dbUser = Environment.GetEnvironmentVariable("DB_USER");
-            string dbPassword =  Environment.GetEnvironmentVariable("DB_PASSWORD");
-            string dbName = Environment.GetEnvironmentVariable("DB_NAME");
+        public APIGatewayProxyResponse GetAllOpportunities(APIGatewayProxyRequest request)
+        {
+            //LambdaLogger.Log("get all active opportunities");
 
             MySqlConnection connection = new MySqlConnection($"server={dbHost};user id={dbUser};password={dbPassword};port=3306;database={dbName};");
             connection.Open();
 
             var cmd = connection.CreateCommand();
-            cmd.CommandText = @"SELECT * FROM `Opportunity` WHERE `opportunityId` = @oppId;";
-            cmd.Parameters.AddWithValue("@oppId", oppId);
-
+            cmd.CommandText = @"SELECT * FROM `V_allOpportunities`";
             ArrayList opportunities = new ArrayList();
 
             MySqlDataReader reader = cmd.ExecuteReader();
             while (reader.Read())
             {
-                Opportunity opportunity = new Opportunity(reader.GetString("opportunityId"), reader.GetString("charityId"),reader.GetString("opportunityName"), reader.GetString("opportunityDescription"));
+                Opportunity opportunity = new Opportunity(
+                    reader.GetInt32("id"), 
+                    reader.GetString("charity"),
+                    reader.GetString("name"), 
+                    reader.GetString("taskType"), 
+                    reader.GetInt32("numVolunteers"), 
+                    reader.GetString("date"), 
+                    reader.GetString("postcode"), 
+                    reader.GetString("address1"), 
+                    reader.GetString("address2"), 
+                    reader.GetString("city"), 
+                    reader.GetString("description"));
                 opportunities.Add(opportunity);
             }
-            
+
             connection.Close();
 
             return new APIGatewayProxyResponse
@@ -51,45 +61,61 @@ namespace AwsDotnetCsharp
                 StatusCode = 200,
             };
         }
+        
+        public APIGatewayProxyResponse SaveOpportunity(APIGatewayProxyRequest request)
+        {
 
-public APIGatewayProxyResponse SaveOpportunity(APIGatewayProxyRequest request) {
+            string requestBody = request.Body;
+            Opportunity o = JsonSerializer.Deserialize<Opportunity>(requestBody);
+            LambdaLogger.Log("Saving Opportunity: " + o.name);
 
-          string requestBody = request.Body;
-          Opportunity o = JsonSerializer.Deserialize<Opportunity>(requestBody);
-          LambdaLogger.Log("Saving Opportunity: " + o.Name);
-
-          return new APIGatewayProxyResponse
-          {
-            Body = "Opportunity Saved",
-            Headers = new Dictionary<string, string>
-            { 
-                { "Content-Type", "application/json" }, 
-                { "Access-Control-Allow-Origin", "*" } 
+            return new APIGatewayProxyResponse
+            {
+                Body = "Opportunity Saved",
+                Headers = new Dictionary<string, string>
+            {
+                { "Content-Type", "application/json" },
+                { "Access-Control-Allow-Origin", "*" }
             },
-            StatusCode = 200,
-          };
-       }
+                StatusCode = 200,
+            };
+        }
 
 
     }
 
+
     public class Opportunity
     {
-        public string Id { get; set; }
-        public string CharityId { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
+        public int id { get; set; }
+        public string charity { get; set; }
+        public string name { get; set; }
+        public string taskType { get; set; }
+        public int numVolunteers { get; set; }
+        public string date { get; set; }
+        public string postcode { get; set; }
+        public string address1 { get; set; }
+        public string address2 { get; set; }
+        public string location { get; set; }
+        public string description { get; set; }
 
-        public Opportunity(string opportunityId, string charityId, string opportunityName, string opportunityDescription)
+        public Opportunity (int Id, string Charity, string Name, string TaskType, int NumVolunteers, string Date, string Postcode, string Address1, string Address2, string City, string Description)
         {
-            Id = opportunityId;
-            CharityId = charityId;
-            Name = opportunityName;
-            Description = opportunityDescription;
+            id = Id;
+            charity = Charity;
+            name = Name;
+            taskType = TaskType;
+            numVolunteers = NumVolunteers;
+            date = Date;
+            postcode = Postcode;
+            address1 = Address1;
+            address2 = Address2;
+            location = City;
+            description = Description;
         }
 
-        public Opportunity() {}
-    } 
+        public Opportunity() { }
+    }
 
     public class Request
     {
